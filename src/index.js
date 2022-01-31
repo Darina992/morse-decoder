27dfa74431958e912d0a89090d05b0a901a3c07f
Darina992 <darina992@gmail.com>
2022-01-31 18:11:55 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arr = expr.split('');
+    let ans = [];
+
+    for(let i = 0;i < arr.length;i++){
+      if(Object.values(MORSE_TABLE).indexOf(arr[i]) !== -1){
+        for(let key in MORSE_TABLE){
+          if(arr[i] === MORSE_TABLE[key]){
+            ans.push(key)
+          }
+        }
+      } else {
+        ans.push(' ')
+      }
+    }
+    let result = [];
+    //console.log(ans)
+    for(let i = 0;i < ans.length;i++){
+    let res = [];
+    for(let j = 0;j < ans[i].length;j++){
+      //console.log(ans[i][j])
+      if(ans[i][j] === '-'){
+        res.push('11');
+      } else if (ans[i][j] === '.'){
+        res.push('10');
+      } else {
+        res.push('**********');
+      }
+    }
+    //console.log(res.join(''));
+    //console.log(res.join('').length)
+    if(res.join('').length < 10){
+      let l = res.join('').length;
+      for(let i = 0;i < (10 - l);i++){
+        res.unshift('0');
+      }
+      //console.log(res)
+      result.push(res.join(''));
+    } else {
+      result.push(res.join(''));
+    }
+
+    }
+    //console.log(result.join(''))
+    return result.join('')
 }
 
 module.exports = {
